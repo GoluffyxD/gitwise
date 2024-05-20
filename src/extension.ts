9b38b9946bca80d2c3b75b10f41d07e52342eb60
Kiran MHM <mhmkiran@gmail.com>
2024-05-19 21:20:25 -0700

    Added sidebar for gitwise

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,57 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { HelloWorldPanel } from './helloworldPanel';
+import { SidebarProvider } from './SidebarProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gitwise" is now active!');
+	const sidebarProvider = new SidebarProvider(context.extensionUri);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gitwise.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Gitwise!');
-	});
+	const item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+	item.text = "$(github-action) Gitwise Explain";
+	item.command = "gitwise.explain";
+	item.show();
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+	  vscode.window.registerWebviewViewProvider(
+		"gitwise-sidebar",
+		sidebarProvider
+	  )
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('gitwise.helloWorld', () => {
+		HelloWorldPanel.createOrShow(context.extensionUri);
+		})
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('gitwise.refresh', async () => {
+		await vscode.commands.executeCommand("workbench.action.closeSidebar");
+		await vscode.commands.executeCommand("workbench.view.extension.gitwise-sidebar-view");
+		// vscode.commands.executeCommand("workbench.action.webview.openDeveloperTools");
+		})
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand("gitwise.explain", () => {
+			const {activeTextEditor} = vscode.window;
+			if (!activeTextEditor){
+				vscode.window.showInformationMessage("No active text editor");
+				return;
+			}
+			const selection = activeTextEditor.document.getText(activeTextEditor.selection);
+			if(selection === "") {
+				vscode.window.showInformationMessage("No selection has been made");
+			} else {
+				sidebarProvider._view?.webview.postMessage({
+					type: 'code-select',
+					value: selection
+				});
+			}
+		})
+	);
 }
 
 // This method is called when your extension is deactivated
---
+++ webviews/components/Sidebar.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import Prism from 'prismjs';
+    let language = "py";
+    let selectedCode = "";
+    let code = "";
+    onMount(()=> {
+        // Prism.highlightAll();
+        // Handle messages sent from the extension to the webview
+        window.addEventListener('message', event => {
+            const message = event.data; // The json data that the extension sent
+            switch (message.type) {
+                case 'code-select':
+                    selectedCode = message.value;
+                    console.log(selectedCode);
+                    break;
+            }
+        });
+    })
+</script>
+
+<style>
+</style>
+  
+<div>
+    <pre class="line-numbers">
+        <code class="language-python">
+            {selectedCode}
+        </code>
+    </pre>
+</div>
+
+<div>
+    {selectedCode}
+</div>
