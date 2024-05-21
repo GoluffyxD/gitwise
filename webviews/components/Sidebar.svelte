5fcca65578907446c43aac4d7e2c3773e96f7388
Kiran MHM <mhmkiran@gmail.com>
2024-05-20 17:42:20 -0700

    Switched to highlightjs

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import Prism from 'prismjs';
+    // import Prism from 'prismjs';
+    import Highlight from "svelte-highlight";
+    import tokyo from "svelte-highlight/styles/tokyo-night-dark";
+    // import typescript from "svelte-highlight/languages/typescript";
+    import python from "svelte-highlight/languages/python";
     import type { CodeSelectionInfo } from "../globals";
     let language = "py";
     let selectedCode = "";
@@ -18,6 +22,7 @@
                     // code_selection = message.info;
                     const code_selection_info: CodeSelectionInfo = JSON.parse(message.value);
                     selectedCode = code_selection_info.code_selected;
+                    code = selectedCode;
                     code_selection = code_selection_info;
                     break;
                 case 'code-explain':
@@ -29,25 +34,19 @@
     })
 </script>
 
+<svelte:head>
+  {@html tokyo}
+</svelte:head>
+
 <style>
 </style>
   
-<div>
-    <pre class="line-numbers">
-        <code class="language-python">
-            {selectedCode}
-        </code>
-    </pre>
-</div>
-
-<div>
-    {selectedCode}
-</div>
-
-<div>
+<h2>Selected Code</h2>
+<Highlight language={python} {code} />
+<!-- <div>
     {code_selection == undefined ? "" : JSON.stringify(code_selection)}
-</div>
-
+</div> -->
+<h2>Code Explanation</h2>
 <div>
     {code_explanation}
 </div>
