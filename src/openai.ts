fb17e7cf03491efab2b6db9bef28038ea8e2c29d
Kiran MHM <mhmkiran@gmail.com>
2024-06-05 02:56:14 -0700

    Updated prompts for edge case without PR Number

--- src/openai.ts
+++ src/openai.ts
@@ -31,21 +31,16 @@ async function getGptResponse(prompt: string): Promise<string> {
 }
 
 async function chat(prInfo: PullRequestInfo, gitDiff: string, selectedCode: string = "") {
-  const prompt = `
-  Given the following pull request (PR) information including the PR title, PR body, PR conversations, and the git diff, please provide a short summary explaining the code change. Make sure you mention the PR title, PR body, and summarise the PR conversations (also mentioning who were involved in the conversation) in your response.PR stands for Pull Request.
-
-  PR Title: ${prInfo.pullRequestTitle}
-  PR Body: ${prInfo.pullRequestBody}
-  PR Conversations: ${prInfo.pullRequestConversations}
-  Git Diff: ${gitDiff}
-
-  Summary:
-  `;
   const prompt2 = `
   Given the following code and pull request (PR) information including the PR title, PR body, PR conversations, and the git diff, please provide a short summary explaining the code change.
-  Split the output into 4 sections. In the first section, explain the code provided in 2 or 3 lines. Briefly explain the git diff explaining the code change in the second section.
-  Summarize the PR Title and Body in the third section and the PR Conversations in the 4th sectino. Split each section with a \n character.
-  Make sure you mention the PR title, PR body, and summarise the PR conversations (also mentioning who were involved in the conversation) in your response.PR stands for Pull Request.
+  Split the output into 4 sections.
+  In the first section, explain only the code provided in 2 or 3 lines. Also explain the code with the git diff as context.
+  In the second section, briefly explain the git diff explaining the code change.
+  While explaining the git diff changes, also explain how the code provided changes and how relevant it is to the other changes in the git diff. 
+  In the third section, summarize the PR Title and Body.
+  In the fourth section, summarize the PR Conversations.
+  Split each section with a \n character.
+  Make sure you mention the PR title, PR body, and summarise the PR conversations (also mentioning who were involved in the conversation) in your response. PR stands for Pull Request.
   Code: ${selectedCode}
   PR Title: ${prInfo.pullRequestTitle}
   PR Body: ${prInfo.pullRequestBody}
@@ -62,6 +57,26 @@ async function chat(prInfo: PullRequestInfo, gitDiff: string, selectedCode: stri
   return outputMessage;
 }
 
+async function chatWithoutPr(gitDiff: string, selectedCode: string = "") {
+  const prompt2 = `
+  Given the following code and the git diff, please provide a short summary explaining the code change.
+  Split the output into 2 sections. In the first section, explain the code provided in 2 or 3 lines.
+  Briefly explain the git diff explaining the code change in the second section.
+  While explaining the git diff changes, also explain how the code provided changes and how relevant it is to the other changes in the git diff.
+  Split each section with a \n character.
+  Code: ${selectedCode}
+  Git Diff: ${gitDiff}
+
+  Summary:
+  `;
+  const response = await getGptResponse(prompt2);
+  const outputMessage: OutputMessage = {
+    explanation: response,
+    pullRequestUrl: ""
+  };
+  return outputMessage;
+}
+
 export async function getGPTExplanation(commitSHA: string, gitDiff: string, owner: string, repo: string, selectedCode: string="") {
   // const owner = 'GoluffyxD';
   // const repo = 'gitwise-test-1';
@@ -69,7 +84,7 @@ export async function getGPTExplanation(commitSHA: string, gitDiff: string, owne
   const result = await getPullRequestNumber(commitSHA, `https://api.github.com/repos/${owner}/${repo}`);
   if (!result) {
     console.log("Pr Number or Issue ID is undefined");
-    return "";
+    return chatWithoutPr(gitDiff, selectedCode);
   }
   console.log("PR ID:", result);
   const {pullRequestId} = result;
